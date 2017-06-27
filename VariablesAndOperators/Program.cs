ea310f610e8fb53b34b26d3f35e12a00ee280425
Paige Miller <paige_9753@hotmail.com>
2017-06-27 15:07:33 -0400

    in class exercises

--- VariablesAndOperators/Program.cs
+++ VariablesAndOperators/Program.cs
@@ -10,6 +10,116 @@ namespace VariablesAndOperators
     {
         static void Main(string[] args)
         {
+            ////initializing new firstname variable
+            //string firstName = "Mike";
+            ////Printing the value of the firstName to the console
+            //Console.WriteLine(firstName);
+
+            //firstName = "Daniel";
+            //Console.WriteLine(firstName);
+
+            ////initailizing new birthMonth variable
+            //string birthMonth = "January";
+            ////Printing the value of the birthMonth to the console
+            //Console.WriteLine(birthMonth);
+
+            //int age = 65;
+            //string why = "because I said so";
+            //char firstIntial = 'L';
+
+            ////floating point types
+            //float floatNumber = 2.164403304897294849383961032f;
+            //Console.WriteLine(floatNumber);
+
+            //double doubleNumber = 2.164403304897294849383961032d;
+            //Console.WriteLine(doubleNumber);
+
+            //decimal decimalNumber = 2.164403304897294849383961032m;
+            //Console.WriteLine(decimalNumber);
+
+            ////Jessica is 23 years old
+            ////Sam is 47 years old
+            //int jessicaAge = 23;
+            //int samAge = 47;
+            //Console.WriteLine(jessicaAge < samAge);
+
+            //Console.WriteLine("What is the name on the reservation?");
+            //string reserveName = Console.ReadLine();
+
+            //Console.WriteLine("How many people are in your party?");
+            //int numberOfPeople = int.Parse(Console.ReadLine());
+
+            //double amountEach = 13.53d;
+            //double totalBill = numberOfPeople * amountEach;
+            
+            //Console.WriteLine("Reservation: " + reserveName);
+            //Console.WriteLine("Total Due: $" + totalBill);
+
+            //Console.WriteLine("How much money do you have?");
+            //int moneyHave = int.Parse(Console.ReadLine());
+            //Console.WriteLine("How much is the price of diapers?");
+            //int diaperCosts = int.Parse(Console.ReadLine());
+
+            //double packsOfDiapers = moneyHave/diaperCosts;
+            //Console.WriteLine(packsOfDiapers);
+
+
+            //Console.WriteLine("How much money did T have?");
+            //int moneyHave = int.Parse(Console.ReadLine());
+            //Console.WriteLine("How much money does T have now?");
+            //int moneyNow = int.Parse(Console.ReadLine());
+
+            //double moneyEarned = moneyNow - moneyHave;
+            //Console.WriteLine(moneyEarned);
+
+            Console.WriteLine("What is your name?");
+            string yourName = Console.ReadLine();
+            Console.WriteLine("What is your address?");
+            string yourAddress = Console.ReadLine();
+            Console.WriteLine("What is your email?");
+            string yourEmail = Console.ReadLine();
+            Console.WriteLine("What is the fiscal year of donation?");
+            int fiscalYear = int.Parse(Console.ReadLine());
+            Console.WriteLine("What was the donation amount?");
+            int donationAmount = int.Parse(Console.ReadLine());
+
+
+            
+            
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
